ede5335a500addb663bc31370cd5f913159c091c
Gareth <g3vans16@gmail.com>
2022-12-16 16:25:02 +0000

    added more controllers

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -7,4 +7,23 @@ import repositories.booking_repository as booking_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
 
-@bookings_blueprint.route("bookings")
+@bookings_blueprint.route("/bookings")
+def bookings():
+    bookings = booking_repository.select_all()
+    return render_template("bookings/index.html", bookings=bookings)
+
+@bookings_blueprint.route("/bookings/new", methods = ['GET'])
+def new_booking():
+    members = member_repository.select_all()
+    gym_classes = gym_class_repository.select_all()
+    return render_template("bookings/new.html", members=members, gym_classes=gym_classes)
+
+@bookings_blueprint.route("/bookings", methods = ['POST'])
+def create_booking():
+    member_id = request.form['member_id']
+    gym_class_id = request.form['gym_class_id']
+    member = member_repository.select(member_id)
+    gym_class = gym_class_repository.select(gym_class_id)
+    booking = Booking(member, gym_class)
+    booking_repository.save(booking)
+    return redirect('/bookings')
--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -6,7 +6,7 @@ import repositories.gym_class_repository as gym_class_repository
 
 gym_classes_blueprint = Blueprint("gym_classes", __name__)
 
-@gym_classes_blueprint.route("gym_classes")
+@gym_classes_blueprint.route("/gym_classes")
 def gym_classes():
     gym_classes = gym_class_repository.select_all()
     return render_template("gym_classes/index.html", gym_classes=gym_classes)
